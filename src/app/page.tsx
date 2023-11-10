af00be7753d1187947140e15b693f0057cda0a3c
Isabela Nery <isabelanery.r@gmail.com>
2023-11-09 22:40:16 -0300

    setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="">
+      <div className="w-full h-[150px] bg-white text-black md:text-[30px]">
+        LUFFY
+      </div>
+    </main>
+  )
+}
